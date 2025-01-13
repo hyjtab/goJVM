238d670e1d06055f5e16f411cad4358d8866cac2
hyjtab <2741521652@qq.com>
2025-01-13 22:47:37 +0800

    chap02 finished

--- jvmgo/ch02/classpath/entry_dir.go
+++ jvmgo/ch02/classpath/entry_dir.go
@@ -1,9 +1,29 @@
 package classpath
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type DirEntry struct {
 	absDir string
 }
 
+// 根据所给路径名建立目录entry
 func newDirEntry(path string) *DirEntry {
+	absDir, err := filepath.Abs(path)
+	if err != nil {
+		panic(err)
+	}
+	return &DirEntry{absDir}
+}
 
+func (self *DirEntry) readClass(className string) ([]byte, Entry, error) {
+	fileName := filepath.Join(self.absDir, className)
+	data, err := os.ReadFile(fileName)
+	return data, self, err
+}
+
+func (self *DirEntry) String() string {
+	return self.absDir
 }
